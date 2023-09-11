741d14d926c9631aea8ad2695f915c347efdf384
dextern31 <140563233+dextern31@users.noreply.github.com>
2023-09-11 16:40:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,51 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const allSuits = ["♦", "♥", "♠", "♣"];
+  const allValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  var cardClass = "heart";
+  var cardSuit = "♥";
+  var cardValue = "K";
+
+  function cardGenerator() {
+    const randSuit = function() {
+      return Math.floor(Math.random() * 4);
+    };
+
+    const randValue = function() {
+      return Math.floor(Math.random() * 13);
+    };
+
+    cardSuit = allSuits[randSuit()];
+
+    cardValue = allValues[randValue()];
+
+    const card = document.querySelector(".card");
+
+    if (cardSuit == "♦") {
+      card.classList.remove(cardClass);
+      cardClass = "diamond";
+      card.classList.add(cardClass);
+    } else if (cardSuit == "♥") {
+      card.classList.remove(cardClass);
+      cardClass = "heart";
+      card.classList.add(cardClass);
+    } else if (cardSuit == "♠") {
+      card.classList.remove(cardClass);
+      cardClass = "spade";
+      card.classList.add(cardClass);
+    } else if (cardSuit == "♣") {
+      card.classList.remove(cardClass);
+      cardClass = "club";
+      card.classList.add(cardClass);
+    }
+    const suitClass = document.getElementsByClassName("suit");
+    for (let elm of suitClass) {
+      elm.innerHTML = cardSuit;
+    }
+    const value = document.querySelector(".value");
+    value.innerHTML = cardValue;
+  }
+  setInterval(cardGenerator, 5000);
 };
